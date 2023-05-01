ee7bce57ed1579f384ccb63d5ac0966bf46f5660
Imad <imadouairem96@gmail.com>
2023-05-01 10:52:14 +0100

    create voiture model

--- rentcarsapp/models.py
+++ rentcarsapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Voiture(models.Model):
+    matricule = models.CharField(max_length=10)
+    marque = models.CharField(max_length=20)
+    model = models.CharField(max_length=20)
+    prix = models.FloatField()
