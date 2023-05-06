701be1bd50463d5f670c224bcf5386fda9cef8f3
Imad <imadouairem96@gmail.com>
2023-05-06 11:11:41 +0100

    switch to mysql db

--- CarRentalDjangoProject/settings.py
+++ CarRentalDjangoProject/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'CarRentalDjangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'car_rental_django',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'PORT': '3306',
+        'HOST': 'localhost'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
